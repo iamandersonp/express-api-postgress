ae87d19d5d374b96c26fbc4e51c5c4433b739a67
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-10-12 11:08:10 +0200

    Added express and set up initial configuration

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Test")
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, resp) =>
+{
+  resp.send('Hello World')
+})
+
+app.listen(port, () =>
+{
+  console.log(`Server is listening on http://localhost:${port}`)
+})
